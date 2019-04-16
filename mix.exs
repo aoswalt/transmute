ea39cf37baa516ce9b2bc8f66e7bba1ac7bca742
Adam Oswalt <email@adamoswalt.com>
2019-04-16 14:00:00 -0500

    Add credo and dialyxir

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Transmute.MixProject do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :transmute,
       version: "0.1.0",
@@ -11,18 +11,16 @@ defmodule Transmute.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
     ]
   end
 end
