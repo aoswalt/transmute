be3c9e3620dabe417feff39d3008d16f37ba83b1
Adam Oswalt <email@adamoswalt.com>
2019-04-16 17:41:27 -0500

    Add `:map_shape` opt to `Transmute.transform/2`

--- lib/transmute.ex
+++ lib/transmute.ex
@@ -1,13 +1,17 @@
 defmodule Transmute do
   @type map_key_fn :: (any -> any)
+  @type map_shape_fn :: (map -> any)
 
-  @type transform_options :: [{:map_key, map_key_fn}]
+  @type transform_options :: [{:map_key, map_key_fn} | {:map_shape, map_shape_fn}]
 
   @spec transform(data :: map, opts :: transform_options) :: any
   def transform(data, opts) do
     map_key = Keyword.get(opts, :map_key, &identity/1)
+    map_shape = Keyword.get(opts, :map_shape, &identity/1)
 
-    Transmute.map_keys(data, map_key)
+    data
+    |> Transmute.map_keys(map_key)
+    |> map_shape.()
   end
 
   @spec map_keys(data :: map, mapper :: map_key_fn) :: map
--- test/transmute_test.exs
+++ test/transmute_test.exs
@@ -24,5 +24,15 @@ defmodule TransmuteTest do
       expected_map = %{"SOME_KEY" => 1, "OTHER_KEY" => 2}
       assert Transmute.transform(start_map, map_key: atom_to_caps) == expected_map
     end
+
+    test "map_shape specifies how to map the shape of the data" do
+      start_data = %{"someKey" => 1, "otherKey" => 2}
+      expected_data = [{"otherKey", 2}, {"someKey", 1}]
+      assert Transmute.transform(start_data, map_shape: &Map.to_list/1) == expected_data
+
+      start_data = %{some_key: 1, other_key: 2}
+      expected_data = [other_key: 2, some_key: 1]
+      assert Transmute.transform(start_data, map_shape: &Map.to_list/1) == expected_data
+    end
   end
 end
