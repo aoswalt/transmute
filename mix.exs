bca5a16d4fc54d9742347cc4748ebaf697fa7011
Adam Oswalt <email@adamoswalt.com>
2019-04-16 14:07:03 -0500

    Add mix_test_watch

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Transmute.MixProject do
   defp deps() do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:mix_test_watch, "~> 0.8", only: [:dev], runtime: false}
     ]
   end
 end
